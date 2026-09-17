fb9033d9855e1080e645f9432b0ac2ce3c8e77b4
Avdhesh yadav <avdheshydv247@gmail.com>
2026-09-17 23:50:10 +0530

    Time: 0 ms (100%), Space: 23.8 MB (72.83%) - LeetHub

---
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -0,0 +1,26 @@
+
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+
+        vector<int> ans;
+        int count = 1;
+
+        for (int i = 0; i < nums.size(); i++) {
+
+            if (i + 1 < nums.size() && nums[i] == nums[i + 1]) {
+                count++;
+            }
+            else {
+                if (count > nums.size() / 3) {
+                    ans.push_back(nums[i]);
+                }
+
+                count = 1;
+            }
+        }
+
+        return ans;
+    }
+};
