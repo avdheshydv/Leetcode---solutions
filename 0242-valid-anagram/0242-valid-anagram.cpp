2ebbdbd76efbfb54df985e377ae635d7b2f565e9
Avdhesh yadav <avdheshydv247@gmail.com>
2026-09-19 12:33:12 +0530

    Time: 0 ms (100%), Space: 9.5 MB (99.25%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -4,12 +4,18 @@ public:
         if(s.length()!=t.length()){
             return false;
         }
-          sort(s.begin(),s.end());
-          sort(t.begin(), t.end());
-
-         if(s==t){
-            return true;
-         }
-         return false;
+       int count[26]={0};
+       for(int i=0; i<s.length(); i++){
+        int index=s[i]-'a'; // index
+        count[index]++;
+       } 
+       for(int i=0; i<t.length(); i++){
+        int index=t[i]-'a';
+        if(count[index]==0){
+            return false;
+        }
+        count[index]--;
+       }
+ return true;
     }
 };
